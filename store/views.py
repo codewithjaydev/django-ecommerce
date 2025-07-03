55b711de93c957062cdabc42e84554e16bde76c0
codewithjaydev <jaydevthapa8@gmail.com>
2025-07-03 20:46:41 +0545

    someupdatation

--- store/views.py
+++ store/views.py
@@ -73,32 +73,3 @@ def updateItem(request):
         orderItem.delete()
     return JsonResponse('Item was added', safe=False)
 
-def processOrder(request):
-    transaction_id=datetime.datetime.now().timestamp()
-    data = json.loads(request.body)
-
-    if request.user.is_authenticated:
-        customer=request.user.customer
-        order, created = Order.objects.get_or_create(customer=customer,complete=False)
-        total= data['form']['total']
-        order.transaction_id= transaction_id
-
-        if total == order.get_cart_total:
-            order.complete=True
-        order.save()
-
-        if order.shipping == True:
-            ShippingAddress.objects.create(
-                customer=customer,
-                order=order,
-                address= data['shipping']['address'],
-                city=data['shipping']['city'],
-                state=data['shipping']['state'],
-                zipcode= data['shipping']['zipcode'],
-
-            )
-
-    else:
-        print("user is not logged in..")
-
-    return JsonResponse('payment complete', safe=False)
